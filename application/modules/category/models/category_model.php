99054f55aab778ee9851f9f617c4d25cb1091b88
shradhasimkhada <shradha.simkhada@gmail.com>
2014-07-10 15:40:53 +0600

    Update category_model.php

--- application/modules/category/models/category_model.php
+++ application/modules/category/models/category_model.php
@@ -16,7 +16,9 @@ class Category_model extends CI_Model {
              return $res;
          }
          
+         
          public function insertcategory($tablename, $data){
+             // function to insert category in table
              $this->db->insert($tablename, $data);
              return $this->db->insert_id();
          }
