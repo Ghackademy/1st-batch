cbbe1e96fcba088dee74417618b849c2da07de95
saminanirmala <samina.tuladhar7@gmail.com>
2014-07-11 08:31:56 +0545

    slight changes made

--- application/modules/category/views/edit.php
+++ application/modules/category/views/edit.php
@@ -5,7 +5,7 @@
     <body>
         <form name="" method="POST">
               Category Title:<input type="text" name="cat_title" value="<?php echo set_value('cat_title');?>"><br>
-             Category Description:<input type="text" name="cat_desc" value="<?php echo set_value('cat_description');?>"><br>
+             Category Description:<input type="text" name="cat_description" value="<?php echo set_value('cat_description');?>"><br>
              <input type="submit" value="Update" name="submit">
         </form>
     </body>
