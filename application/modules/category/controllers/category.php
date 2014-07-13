9da0acf8fd3da7581ca464ad11c32b534a562791
shradhasimkhada <shradha.simkhada@gmail.com>
2014-07-13 14:48:46 +0600

    product list created

--- application/modules/category/controllers/category.php
+++ application/modules/category/controllers/category.php
@@ -1,11 +1,11 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class category extends MX_Controller {
-const table = "tb_category";    
+class category extends MX_Controller {   
     Public function __construct(){
         parent::__construct();
         $this->load->model('category_model'); // load category model
         $this->load->library('session');
+     
         
               
     }
@@ -15,7 +15,7 @@ const table = "tb_category";
       */
     public function categorylist(){ 
         
-         $data['allcategory']= $this->category_model->getAllCategory(category::table);
+         $data['allcategory']= $this->category_model->getAllCategory();
 
         $this->load->view('categorylist',$data);
     }
@@ -24,13 +24,16 @@ const table = "tb_category";
      * returns data insert
      */
     public function add(){  
+        $this->load->library('form_validation');
+
+      
         if ($_POST){
             $data= array(
                 'cat_title'=>  $this->input->post('cat_title'),
                 'cat_description'=>  $this->input->post('cat_desc')
             );
-        
-        $this->category_model->insertCategory(category::table,$data);
+          $this->form_validation->set_rules('cat_title', 'cat_title', 'required');
+        $this->category_model->insertCategory($data);
         redirect('category/categorylist');
         
         }
