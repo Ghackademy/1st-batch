162d60609e714b45bc5aebcffbc0170d5f13bb0c
saminanirmala <samina.tuladhar7@gmail.com>
2014-07-24 14:05:17 +0545

    routes added

--- application/config/routes.php
+++ application/config/routes.php
@@ -37,7 +37,7 @@
 | in the URL cannot be matched to a valid route.
 |
 */
-$route['default_controller'] = "welcome";
+$route['default_controller'] = "user/welcome";
 $route['404_override'] = '';
 
 
