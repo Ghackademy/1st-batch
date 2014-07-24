033a92994b44d4c3933517c5528288a57925bb8b
saminanirmala <samina.tuladhar7@gmail.com>
2014-07-24 13:57:48 +0545

    index page added

--- application/modules/user/views/index.php
+++ application/modules/user/views/index.php
@@ -1,31 +1,126 @@
-<h1><?php echo lang('index_heading');?></h1>
-<p><?php echo lang('index_subheading');?></p>
+<!DOCTYPE html>
 
-<div id="infoMessage"><?php echo $message;?></div>
 
-<table cellpadding=0 cellspacing=10>
-	<tr>
-		<th><?php echo lang('index_fname_th');?></th>
-		<th><?php echo lang('index_lname_th');?></th>
-		<th><?php echo lang('index_email_th');?></th>
-		<th><?php echo lang('index_groups_th');?></th>
-		<th><?php echo lang('index_status_th');?></th>
-		<th><?php echo lang('index_action_th');?></th>
-	</tr>
-	<?php foreach ($users as $user):?>
-		<tr>
-			<td><?php echo $user->first_name;?></td>
-			<td><?php echo $user->last_name;?></td>
-			<td><?php echo $user->email;?></td>
-			<td>
-				<?php foreach ($user->groups as $group):?>
-					<?php echo anchor("user/edit_group/".$group->id, $group->name) ;?><br />
-                <?php endforeach?>
-			</td>
-			<td><?php echo ($user->active) ? anchor("user/deactivate/".$user->id, lang('index_active_link')) : anchor("user/activate/". $user->id, lang('index_inactive_link'));?></td>
-			<td><?php echo anchor("user/edit_user/".$user->id, 'Edit') ;?></td>
-		</tr>
-	<?php endforeach;?>
-</table>
+<html>
+    <head>
+        <title>Index</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="<?php echo base_url();?>assests/css/bootstrap.min.css" type="text/css" media="screen">
+        <link rel="stylesheet" href="<?php echo base_url();?>assests/css/font-awesome.min.css" type="text/css">
+        <link rel="stylesheet" href="<?php echo base_url();?>assests/css/style.css" type="text/css">
+        
+    <body>
+           
+   <nav class="navbar-default" role="navigation">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="nav1">
+          <div class="nav1Content">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">Logo</a>
+    </div>
 
-<p><?php echo anchor('user/create_user', lang('index_create_user_link'))?> | <?php echo anchor('user/create_group', lang('index_create_group_link'))?></p>
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        
+        
+        <div class="searchForm">
+  <div class="col-lg-4 col-xs-12 col-sm-12 col-md-4">
+    <div class="headerform input-group">
+        
+      <input type="text" class="form-control">
+      <span class="input-group-btn">
+        <button class="searchbtn glyphicon glyphicon-search" type="button"></button>
+      </span>
+    </div><!-- /input-group -->
+  </div><!-- /.col-lg-6 -->
+        </div>     
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#">My Account</a></li>
+        <li><a href="#">My Wishlist</a></li>
+        <li><a href="#">My Cart</a></li>
+        <li><a href="#">Login</a></li>
+        
+      </ul>
+         </div>
+    </div><!-- /.navbar-collapse -->
+ <!-- /.container-fluid -->
+    </div>
+  </div>
+</nav>
+        <div class="clearfix"></div>
+        <!--nav2-->
+        <div class="nav2Wrapper">
+            <div class="nav2Contents">
+        <!--category-->
+        <div class="btn-group">
+  <div class="nav2Menu" data-toggle="dropdown">Categories </div>
+  <ul class="nav2ScrollDwn dropdown-menu" role="menu">
+    <li><a href="#">Action</a></li>
+    <li><a href="#">Another action</a></li>
+    <li><a href="#">Something else here</a></li>
+    <li><a href="#">Separated link</a></li>
+  </ul>
+</div>
+      <!--brand-->
+       <div class="btn-group">
+  <div class="nav2Menu" data-toggle="dropdown">Brand</div>
+  <ul class="nav2ScrollDwn dropdown-menu" role="menu">
+    <li><a href="#">Action</a></li>
+    <li><a href="#">Another action</a></li>
+    <li><a href="#">Something else here</a></li>
+    <li><a href="#">Separated link</a></li>
+  </ul>
+</div>
+      <!--sales-->
+       <div class="btn-group">
+  <div class="nav2Menu" data-toggle="dropdown">Sales </div>
+  <ul class="nav2ScrollDwn dropdown-menu" role="menu">
+    <li><a href="#">Action</a></li>
+    <li><a href="#">Another action</a></li>
+    <li><a href="#">Something else here</a></li>
+  </ul>
+</div>
+      <!--new product-->
+       <div class="btn-group">
+  <div class="nav2Menu" data-toggle="dropdown">New products </div>
+  <ul class="nav2ScrollDwn dropdown-menu" role="menu">
+    <li><a href="#">Action</a></li>
+    <li><a href="#">Another action</a></li>
+    <li><a href="#">Something else here</a></li>
+  </ul>
+</div>
+      <!--contactus-->
+       <div class="btn-group">
+  <div class="nav2Menu" data-toggle="dropdown">Contact us</div>
+  <ul class="nav2ScrollDwn dropdown-menu" role="menu">
+    <li><a href="#">Action</a></li>
+    <li><a href="#">Another action</a></li>
+    <li><a href="#">Something else here</a></li>
+  </ul>
+</div>
+      <!--about us-->
+       <div class="btn-group">
+     <div class="nav2Menu" data-toggle="dropdown">About us </div>
+  <ul class="nav2ScrollDwn dropdown-menu" role="menu">
+    <li><a href="#">Action</a></li>
+    <li><a href="#">Another action</a></li>
+    <li><a href="#">Something else here</a></li>
+  </ul>
+</div>
+
+            </div> <!--content close-->
+            <div class="clearfix"></div>
+        </div>
+
+        <script src="<?php echo base_url();?>assests/js/jquery.js"></script>
+        <script src="<?php echo base_url();?>assests/js/bootstrap.min.js"></script>
+    </body>
+</html>
