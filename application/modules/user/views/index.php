bd73695d7e9479052dc6a0c307a6562ae4e57d25
Nirmala100 <shrestha.nirmala100@gmail.com>
2014-07-26 18:21:11 +0545

    backend integration is done

--- application/modules/user/views/index.php
+++ application/modules/user/views/index.php
@@ -8,85 +8,117 @@
         <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
         <link rel="stylesheet" type="text/css" href="assets/css/font-awesome.min.css">
         <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
-        <script src="lean-slider.js">
-            $(document).ready(function() {
-    $('#slider').leanSlider();
-});
-directionNavPrevBuilder: function(prevText){
-    return '<a href="#" class="lean-slider-prev">'+ prevText +'</a>';        
-}
-
-directionNavNextBuilder: function(nextText){
-    return '<a href="#" class="lean-slider-prev">'+ nextText +'</a>';        
-}
-
-controlNavBuilder: function(index, slide){
-    return '<a href="#" class="lean-slider-control-nav">'+ (index + 1) +'</a>';
-}
-        </script>
+ 
 <link rel="stylesheet" href="lean-slider.css" type="text/css" />
     </head>
     <body>
         <!--headerpart-->
-        <section id="topNavigation">
-            <div class="container">
-            <nav class="navbar navbar-default" role="navigation">
-  <!-- Brand and toggle get grouped for better mobile display -->
-  <div class="navbar-header">
-    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-      <span class="sr-only">Toggle navigation</span>
-      <span class="icon-bar"></span>
-      <span class="icon-bar"></span>
-      <span class="icon-bar"></span>
-    </button>
-    <a class="navbar-brand" href="#">Brand</a>
-  </div>
-  <!-- Collect the nav links, forms, and other content for toggling -->
-  <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-    <ul class="nav navbar-nav">
-      <li class="active"><a href="#">Link</a></li>
-      <li><a href="#">Link</a></li>
-      <li class="dropdown">
-        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
-        <ul class="dropdown-menu">
-          <li><a href="#">Action</a></li>
-          <li><a href="#">Another action</a></li>
-          <li><a href="#">Something else here</a></li>
-          <li class="divider"></li>
-          <li><a href="#">Separated link</a></li>
-          <li class="divider"></li>
-          <li><a href="#">One more separated link</a></li>
-        </ul>
-      </li>
-    </ul>
-    <form class="navbar-form navbar-left" role="search">
-      <div class="form-group">
-        <input type="text" class="form-control" placeholder="Search">
-      </div>
-      <button type="submit" class="btn btn-default">Submit</button>
-    </form>
-    <ul class="nav navbar-nav navbar-right">
-      <li><a href="#">Link</a></li>
-      <li class="dropdown">
-        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
-        <ul class="dropdown-menu">
-          <li><a href="#">Action</a></li>
-          <li><a href="#">Another action</a></li>
-          <li><a href="#">Something else here</a></li>
-          <li class="divider"></li>
-          <li><a href="#">Separated link</a></li>
-        </ul>
-      </li>
-    </ul>
-  </div><!-- /.navbar-collapse -->
-</nav>
-                <div class="clearfix"></div>
+         <nav class="navbar-default" role="navigation">
+            <div class="container-fluid">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="nav1">
+                    <div class="nav1Content">
+                        <div class="navbar-header">
+                            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                                <span class="sr-only">Toggle navigation</span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                            </button>
+                            <a class="navbar-brand" href="#">Logo</a>
+                        </div>
+
+                        <!-- Collect the nav links, forms, and other content for toggling -->
+                        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+
+
+                            <div class="searchForm">
+                                <div class="col-lg-4 col-xs-12 col-sm-12 col-md-4">
+                                    <div class="headerform input-group">
+
+                                        <input type="text" class="form-control">
+                                        <span class="input-group-btn">
+                                            <button class="searchbtn glyphicon glyphicon-search" type="button">
+                                            </button>
+                                        </span>
+                                    </div><!-- /input-group -->
+                                </div><!-- /.col-lg-6 -->
+                            </div>     
+                            <ul class="nav navbar-nav navbar-right">
+                                <li><a href="#">My Account</a></li>
+                                <li><a href="#">My Wishlist</a></li>
+                                <li><a href="#">My Cart</a></li>
+                                <li><a href="#">Login</a></li>
+
+                            </ul>
+                        </div>
+                    </div><!-- /.navbar-collapse -->
+                    <!-- /.container-fluid -->
+                </div>
             </div>
-        </section>
+        </nav>
+        <div class="clearfix"></div>
+        <!--nav2-->
+        <div class="nav2Wrapper">
+            <div class="nav2Contents">
+                <!--category-->
+                <div class="btn-group">
+                    <div class="nav2Menu" data-toggle="dropdown">Categories </div>
+                    <ul class="nav2ScrollDwn dropdown-menu" role="menu">
+                        <li><a href="#">Action</a></li>
+                        <li><a href="#">Another action</a></li>
+                        <li><a href="#">Something else here</a></li>
+                        <li><a href="#">Separated link</a></li>
+                    </ul>
+                </div>
+                <!--brand-->
+                <div class="btn-group">
+                    <div class="nav2Menu" data-toggle="dropdown">Brand</div>
+                    <ul class="nav2ScrollDwn dropdown-menu" role="menu">
+                        <li><a href="#">Action</a></li>
+                        <li><a href="#">Another action</a></li>
+                        <li><a href="#">Something else here</a></li>
+                        <li><a href="#">Separated link</a></li>
+                    </ul>
+                </div>
+                <!--sales-->
+                <div class="btn-group">
+                    <div class="nav2Menu" data-toggle="dropdown">Sales </div>
+                    <ul class="nav2ScrollDwn dropdown-menu" role="menu">
+                        <li><a href="#">Action</a></li>
+                        <li><a href="#">Another action</a></li>
+                        <li><a href="#">Something else here</a></li>
+                    </ul>
+                </div>
+                <!--new product-->
+                <div class="btn-group">
+                    <div class="nav2Menu" data-toggle="dropdown">New products </div>
+                    <ul class="nav2ScrollDwn dropdown-menu" role="menu">
+                        <li><a href="#">Action</a></li>
+                        <li><a href="#">Another action</a></li>
+                        <li><a href="#">Something else here</a></li>
+                    </ul>
+                </div>
+                <!--contactus-->
+                <div class="btn-group">
+                    <div class="nav2Menu" data-toggle="dropdown">Contact us</div>
+                    
+                </div>
+                <!--about us-->
+                <div class="btn-group">
+                    <div class="nav2Menu" data-toggle="dropdown">About us </div>
+                  
+                </div>
+
+            </div> <!--content close-->
+            <div class="clearfix"></div>
+        </div>
+
+        <div class="clearfix"></div>
         <section id="second">
             <div class="container">
                 <div class="row">
-                <div class="col-md-3 col-xs-3 col-sm-3 col-lg-3">
+                <div class="col-md-3 col-xs-12 col-sm-6 col-lg-3">
                     <div id="sidebar">
                         
                         <div id="sidebar_heading_group">
@@ -94,14 +126,14 @@ controlNavBuilder: function(index, slide){
                            <div id="sidebar_heading">Categories</div><div class="clearfix"></div>
                         </div>
                         <ul class="sidebarlist">
-						<?php foreach($allcategory as $c): ?>
+                                    <?php foreach($allcategory as $c): ?>
                                     <li><?php echo $c['cat_title'];?></li>
                                 
 						<?php endforeach; ?>
-						</ul>
+                                </ul>             
                                 </div>
                 </div>
-                    <div class="col-md-9 col-xs-9 col-sm-9 col-lg-9">
+                    <div class="col-md-9 col-xs-12 col-sm-12 col-lg-9">
                         <div class="imageslider">
                            <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
   <!-- Indicators -->
@@ -116,7 +148,7 @@ controlNavBuilder: function(index, slide){
         <img src="assets/images/bg1-bannersequence.jpg" alt="...">
        
       <div class="carousel-caption">
-       this is girl
+       arnav izzo
       </div>
     </div>
    <div class="item">
@@ -144,63 +176,106 @@ controlNavBuilder: function(index, slide){
         </section>
         <section id="third">
             <div class="container">
-                <hr color="orange">
                 <div class="row">
-                    <div class="col-md-3 col-sm-3 col-xs-3 col-lg-3">
-                        <div class="advertisement">
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                   <hr color="orange">
+                   <div class="col-md-3 col-sm-6 col-xs-12 col-lg-3">
+                       
+                        <div class="advertisement row">
                             <img src="assets/images/block1.jpg" alt="advertisement 1">
                         </div>
                     </div>
-                     <div class="col-md-3 col-sm-3 col-xs-3 col-lg-3">
+                     <div class="col-md-3 col-sm-6 col-xs-12 col-lg-3 ">
                         <div class="advertisement">
                             <img src="assets/images/block2.jpg" alt="advertisement 1">
                         </div>
                     </div>
-                    <div class="col-md-3 col-sm-3 col-xs-3 col-lg-3">
+                    <div class="col-md-3 col-sm-6 col-xs-12 col-lg-3">
                         <div class="advertisement">
-                            <img src="assetsimages/block3.jpg" alt="advertisement 1">
+                            <img src="assets/images/block3.jpg" alt="advertisement 1">
                         </div>
                     </div>
-                    <div class="col-md-3 col-sm-3 col-xs-3 col-lg-3">
+                    <div class="col-md-3 col-sm-6 col-xs-12 col-lg-3">
                         <div class="advertisement">
                             <img src="assets/images/banner1-images.png" alt="advertisement 1">
                         </div>
                     </div>
+                 <hr color="orange"> 
+               </div>
                 </div>
-                <hr color="orange">
             </div>
         </section>
         <section id="fourth">
             <div class="container">
                 <div class="row">
-                    <div class="col-md-3 contentLeft">
+                    <div class="col-md-3 col-xs-12 col-sm-12 col-lg-3 contentLeft">
                         <img src="assets/images/banner-left.jpg">
                     </div>
-                    <div class="col-md-9 contentRight">
-                        <div class="heading">Featured Products</div>
-                        <div class="col-xs-4 col-md-4 col-sm-4 col-lg-4 product">
+                    <div class="col-md-9 col-xs-12 col-sm-12 col-lg-9 contentRight">
+                       
+                        <div class="heading ">Featured Products</div>
+                        <div class="col-xs-12 col-md-4 col-sm-6 col-lg-4 product">
                           
                           <img src="assets/images/13.jpg" class="product_img"height="250" >
                           <p class="pdesc">This is converse.<br>$30.99<br>
                               <i class="fa fa-star star"></i>
                           <i class="fa fa-star star"></i>
-                          <i class="fa fa-star star"></i>
-                          <i class="fa fa-star star"></i></p>
-                          
-                        </div>
+                         
+                        </p>
+                       <span class="addtocart"><i class="fa fa-shopping-cart"></i>ADD TO CART</span>  
+                       <span class="addtocart"><i class="fa fa-heart"></i></span>
+                       </div>
                                      
-                        <div class="col-xs-4 col-md-4 col-sm-4 col-lg-4 product">
+                        <div class="col-xs-12 col-md-4 col-sm-6 col-lg-4 product">
                            
-                        <img src="assets/images/30.jpg" class="product_img" height="250" width="250">
-                          
-                    </div>
-                        <div class="col-xs-4 col-md-4 col-sm-4 col-lg-4 product">
+                        <img src="images/30.jpg" class="product_img" height="250" width="250">
+                         <p class="pdesc">Black Jacket.<br>$30.99<br>
+                              <i class="fa fa-star star"></i>
+                          <i class="fa fa-star star"></i>
+                          <i class="fa fa-star star"></i>
+                           <i class="fa fa-star star"></i>
+                        </p>
+                       <span class="addtocart"><i class="fa fa-shopping-cart"></i>ADD TO CART</span> 
+                       
+                       <span class="addtocart"><i class="fa fa-heart"></i></span>
+                        </div>                       
+                   
+                        <div class="col-xs-12 col-md-4 col-sm-6 col-lg-4 product">
                            
-                          <img src="assets/images/20_1.jpg" class="product_img"height="250">
+                          <img src="images/20_1.jpg" class="product_img"height="250">
+                           <p class="pdesc">A brown bag.<br>$30.99<br>
+                              <i class="fa fa-star star"></i>
+                          <i class="fa fa-star star"></i>
+                           <i class="fa fa-star star"></i>
+                        </p>
+                       <span class="addtocart"><i class="fa fa-shopping-cart"></i>ADD TO CART</span>  
+                       <span class="addtocart"><i class="fa fa-heart"></i></span>
                     </div>
-                </div>
+               
+                    </div>
             </div>
             </div>
+        </section>
+        <section id="fifth">
+            
+                    <footer>
+                       
+                        <div class="footer1"></div>
+                        <div class="footer2">
+                             <div class="container">
+                             <div class="row">
+                            <div class="col-md-4 col-lg-4 col-xs-12 col-sm-12 footernoteleft">
+                                G-Hackademy All rights reserved
+                                <br>Designed by sylvia
+                            </div>
+                             <div class="col-md-4 col-lg-4 col-sm-12 col-xs-12 footernoteright">
+                               Special|New Products|Top Sellers|Our Stores
+                            </div>
+                        </div>
+                             </div>
+                        </div>
+                    </footer>
+            
         </section>
         <!--Javascript Section-->
         <script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
