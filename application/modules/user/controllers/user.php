ba22f803cbc536c473db27ec05e6991a611b9ac1
saminanirmala <samina.tuladhar7@gmail.com>
2014-07-29 21:00:39 +0545

    user controller updated

--- application/modules/user/controllers/user.php
+++ application/modules/user/controllers/user.php
@@ -456,8 +456,8 @@ class User extends CI_Controller {
 	//create a new user
 	function create_user()
 	{
-		$this->data['title'] = "Create User";
-                $this->data['groupname'] = $this->ion_auth_model->get_groups();
+		$data['title'] = "Create User";
+                $data['groupname'] = $this->ion_auth_model->get_groups();
               
 
 //		if (!$this->ion_auth->logged_in() || !$this->ion_auth->is_admin())
@@ -472,9 +472,6 @@ class User extends CI_Controller {
 		$this->form_validation->set_rules('last_name', $this->lang->line('create_user_validation_lname_label'), 'required|xss_clean');
 		$this->form_validation->set_rules('email', $this->lang->line('create_user_validation_email_label'), 'required|valid_email|is_unique['.$tables['users'].'.email]');
 		$this->form_validation->set_rules('phone', $this->lang->line('create_user_validation_phone_label'), 'required|xss_clean');
-                $this->form_validation->set_rules('country', $this->lang->line('create_user_validation_zipcode_label'), 'required|xss_clean');
-                $this->form_validation->set_rules('zipcode', $this->lang->line('create_user_validation_poboxno_label'), 'required|xss_clean');
-                $this->form_validation->set_rules('poboxno', $this->lang->line('create_user_validation_country_label'), 'required|xss_clean');
 		$this->form_validation->set_rules('company', $this->lang->line('create_user_validation_company_label'), 'required|xss_clean');
 		$this->form_validation->set_rules('password', $this->lang->line('create_user_validation_password_label'), 'required|min_length[' . $this->config->item('min_password_length', 'ion_auth') . ']|max_length[' . $this->config->item('max_password_length', 'ion_auth') . ']|matches[password_confirm]');
 		$this->form_validation->set_rules('password_confirm', $this->lang->line('create_user_validation_password_confirm_label'), 'required');
@@ -489,12 +486,9 @@ class User extends CI_Controller {
 				'first_name' => $this->input->post('first_name'),
 				'last_name'  => $this->input->post('last_name'),
 				'company'    => $this->input->post('company'),
-                                'country' => $this->input->post('country'),
-                                'zipcode' => $this->input->post('zipcode'),
-                                 'poboxno' => $this->input->post('poboxno'),
 				'phone'      => $this->input->post('phone'),
-                                 'user_type' =>$this->input->post('user_type')
-//                                'user_image' =>  $this->do_upload()
+               'user_type' =>$this->input->post('user_type')
+//                               
 			);
                        
 		}
@@ -511,75 +505,75 @@ class User extends CI_Controller {
 			//set the flash data error message if there is one
 			$this->data['message'] = (validation_errors() ? validation_errors() : ($this->ion_auth->errors() ? $this->ion_auth->errors() : $this->session->flashdata('message')));
 
-			$this->data['first_name'] = array(
-				'name'  => 'first_name',
-				'id'    => 'first_name',
-				'type'  => 'text',
-				'value' => $this->form_validation->set_value('first_name'),
-			);
-			$this->data['last_name'] = array(
-				'name'  => 'last_name',
-				'id'    => 'last_name',
-				'type'  => 'text',
-				'value' => $this->form_validation->set_value('last_name'),
-			);
-			$this->data['email'] = array(
-				'name'  => 'email',
-				'id'    => 'email',
-				'type'  => 'text',
-				'value' => $this->form_validation->set_value('email'),
-			);
-                        
-                        $this->data['country'] = array(
-				'name'  => 'country',
-				'id'    => 'country',
-				'type'  => 'text',
-				'value' => $this->form_validation->set_value('country'),
-			);
-                           $this->data['zipcode'] = array(
-				'name'  => 'zipcode',
-				'id'    => 'zipcode',
-				'type'  => 'text',
-				'value' => $this->form_validation->set_value('zipcode'),
-			);
-                              $this->data['poboxno'] = array(
-				'name'  => 'poboxno',
-				'id'    => 'poboxno',
-				'type'  => 'text',
-				'value' => $this->form_validation->set_value('poboxno'),
-			);
-			$this->data['company'] = array(
-				'name'  => 'company',
-				'id'    => 'company',
-				'type'  => 'text',
-				'value' => $this->form_validation->set_value('company'),
-			);
-			$this->data['phone'] = array(
-				'name'  => 'phone',
-				'id'    => 'phone',
-				'type'  => 'text',
-				'value' => $this->form_validation->set_value('phone'),
-			);
-			$this->data['password'] = array(
-				'name'  => 'password',
-				'id'    => 'password',
-				'type'  => 'password',
-				'value' => $this->form_validation->set_value('password'),
-			);
-			$this->data['password_confirm'] = array(
-				'name'  => 'password_confirm',
-				'id'    => 'password_confirm',
-				'type'  => 'password',
-				'value' => $this->form_validation->set_value('password_confirm'),
-			);
-                        	$this->data['image'] = array(
-				'name'  => 'image',
-				'id'    => 'image',
-				'type'  => 'file',
-				'value' => $this->form_validation->set_value('image'),
-			);
+//			$this->data['first_name'] = array(
+//				'name'  => 'first_name',
+//				'id'    => 'first_name',
+//				'type'  => 'text',
+//				'value' => $this->form_validation->set_value('first_name'),
+//			);
+//			$this->data['last_name'] = array(
+//				'name'  => 'last_name',
+//				'id'    => 'last_name',
+//				'type'  => 'text',
+//				'value' => $this->form_validation->set_value('last_name'),
+//			);
+//			$this->data['email'] = array(
+//				'name'  => 'email',
+//				'id'    => 'email',
+//				'type'  => 'text',
+//				'value' => $this->form_validation->set_value('email'),
+//			);
+//                        
+//                        $this->data['country'] = array(
+//				'name'  => 'country',
+//				'id'    => 'country',
+//				'type'  => 'text',
+//				'value' => $this->form_validation->set_value('country'),
+//			);
+//                           $this->data['zipcode'] = array(
+//				'name'  => 'zipcode',
+//				'id'    => 'zipcode',
+//				'type'  => 'text',
+//				'value' => $this->form_validation->set_value('zipcode'),
+//			);
+//                              $this->data['poboxno'] = array(
+//				'name'  => 'poboxno',
+//				'id'    => 'poboxno',
+//				'type'  => 'text',
+//				'value' => $this->form_validation->set_value('poboxno'),
+//			);
+//			$this->data['company'] = array(
+//				'name'  => 'company',
+//				'id'    => 'company',
+//				'type'  => 'text',
+//				'value' => $this->form_validation->set_value('company'),
+//			);
+//			$this->data['phone'] = array(
+//				'name'  => 'phone',
+//				'id'    => 'phone',
+//				'type'  => 'text',
+//				'value' => $this->form_validation->set_value('phone'),
+//			);
+//			$this->data['password'] = array(
+//				'name'  => 'password',
+//				'id'    => 'password',
+//				'type'  => 'password',
+//				'value' => $this->form_validation->set_value('password'),
+//			);
+//			$this->data['password_confirm'] = array(
+//				'name'  => 'password_confirm',
+//				'id'    => 'password_confirm',
+//				'type'  => 'password',
+//				'value' => $this->form_validation->set_value('password_confirm'),
+//			);
+//                        	$this->data['image'] = array(
+//				'name'  => 'image',
+//				'id'    => 'image',
+//				'type'  => 'file',
+//				'value' => $this->form_validation->set_value('image'),
+//			);
 
-			$this->_render_page('create_user', $this->data);
+			$this->_render_page('register', $data);
 		}
 	}
       
