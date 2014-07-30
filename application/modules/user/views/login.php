7e25b4a20ea17fb5cb98c34f3bfb6f7ffea848b6
sylviashakya <shakyasylvia@gmail.com>
2014-07-30 15:42:14 +0545

    updated

--- application/modules/user/views/login.php
+++ application/modules/user/views/login.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <html>
     <head>
         <title>login</title>
@@ -15,4 +16,23 @@
             <input type="submit" name="submit">
         </form>
     </body>
+=======
+<html>
+    <head>
+        <title>login</title>
+    </head>
+    <body>
+        <form method="post" action="">
+
+=======
+       <?php  $msg = $this->session->flashdata('msg');
+                //$res = $this->session->userdata('res');
+                if(!empty($msg)) { echo  $msg;} ?> <br>
+
+            Username:<input type="text" name="email" placeholder="enter your email"><br>
+            Password:<input type="password" name="password" placeholder="enter your passsword"><br>
+            <input type="submit" name="submit">
+        </form>
+    </body>
+>>>>>>> origin
 </html>
