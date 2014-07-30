f920bd1cd1b82daab289fa8319001801ee754f8d
saminanirmala <samina.tuladhar7@gmail.com>
2014-07-30 14:43:58 +0545

    controller user added

--- application/modules/user/controllers/user.php
+++ application/modules/user/controllers/user.php
@@ -458,6 +458,7 @@ class User extends CI_Controller {
 	{
 		$data['title'] = "Create User";
                 $data['groupname'] = $this->ion_auth_model->get_groups();
+             
               
 
 //		if (!$this->ion_auth->logged_in() || !$this->ion_auth->is_admin())
@@ -471,7 +472,6 @@ class User extends CI_Controller {
 		$this->form_validation->set_rules('first_name', $this->lang->line('create_user_validation_fname_label'), 'required|xss_clean');
 		$this->form_validation->set_rules('last_name', $this->lang->line('create_user_validation_lname_label'), 'required|xss_clean');
 		$this->form_validation->set_rules('email', $this->lang->line('create_user_validation_email_label'), 'required|valid_email|is_unique['.$tables['users'].'.email]');
-		$this->form_validation->set_rules('phone', $this->lang->line('create_user_validation_phone_label'), 'required|xss_clean');
 		$this->form_validation->set_rules('company', $this->lang->line('create_user_validation_company_label'), 'required|xss_clean');
 		$this->form_validation->set_rules('password', $this->lang->line('create_user_validation_password_label'), 'required|min_length[' . $this->config->item('min_password_length', 'ion_auth') . ']|max_length[' . $this->config->item('max_password_length', 'ion_auth') . ']|matches[password_confirm]');
 		$this->form_validation->set_rules('password_confirm', $this->lang->line('create_user_validation_password_confirm_label'), 'required');
@@ -486,8 +486,7 @@ class User extends CI_Controller {
 				'first_name' => $this->input->post('first_name'),
 				'last_name'  => $this->input->post('last_name'),
 				'company'    => $this->input->post('company'),
-				'phone'      => $this->input->post('phone'),
-               'user_type' =>$this->input->post('user_type')
+                              'group_id' =>$this->input->post('user_type')
 //                               
 			);
                        
@@ -497,7 +496,7 @@ class User extends CI_Controller {
 			//check to see if we are creating the user
 			//redirect them back to the admin page
 			$this->session->set_flashdata('message', $this->ion_auth->messages());
-			redirect("user/userview", 'refresh');
+			redirect("user/login", 'refresh');
 		}
 		else
 		{
