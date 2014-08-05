99821232305e920a0bc943f0cbc574e7b45e664d
kritinemkul <nemkulk@gmail.com>
2014-08-05 21:33:57 +0545

    files added

--- application/modules/product/views/productList.php
+++ application/modules/product/views/productList.php
@@ -4,9 +4,9 @@
         <title>Bootstrap practice test day1</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width initial-scale=1.0">
-        <link rel="stylesheet" type="text/css" href="css/style.css">
-        <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-        <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/style.css">
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/font-awesome.min.css">
         <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
        
 <link rel="stylesheet" href="lean-slider.css" type="text/css" />
@@ -167,7 +167,7 @@
             
         </section>
         <!--Javascript Section-->
-        <script type="text/javascript" src="js/bootstrap.min.js"></script>
-        <script type="text/javascript" src="js/jquery.js"></script>
+        <script type="text/javascript" src="<?php echo base_url(); ?>assets/js/bootstrap.min.js"></script>
+        <script type="text/javascript" src="<?php echo base_url(); ?>assets/js/jquery.js"></script>
     </body>
 </html>
