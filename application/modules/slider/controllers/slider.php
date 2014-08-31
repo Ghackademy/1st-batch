c61796ab2ab2e5e3ff12230d87d944c1ddb779ba
saminanirmala <samina.tuladhar7@gmail.com>
2014-08-31 22:09:48 +0545

    slider updated

---
+++ application/modules/slider/controllers/slider.php
@@ -0,0 +1,34 @@
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+class Slider extends MX_Controller {
+   const Table = 'tb_slider';
+
+    public function __construct() {
+        parent::__construct();
+        $this->load->model('slider_model');
+       $this->load->library('session');
+        
+   }
+      
+        
+        public function addMedia(){
+            $data['frontend_image'] = $this->slider_model->getimage();
+        if ($_POST) {
+            $this->load->module('product');
+            $image = $this->product->do_upload();
+
+            $data = array('frontend_image' => $image
+            );
+            $this->slider_model->addimage($data);
+
+            redirect('home/index', $data);
+        } else {
+            $this->load->view('user/media', $data);
+        }
+    }
+
+}
+?>
